96fede192599acc39b498b8f416431e7b9cc5065
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-08-07 15:35:54 +0700

    660612137

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,7 @@ export default function RandomUserPage() {
   // annotate type for users state variable
   const [users, setUsers] = useState<UserCardProps[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [genAmount, setGenAmount] = useState(1);
-
-  useEffect(() => {
-    const savedGenAmount = localStorage.getItem("genAmount");
-    if (savedGenAmount) {
-      setGenAmount(Number(savedGenAmount));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("genAmount", genAmount.toString());
-  }, [genAmount]);
+  const [genAmount, setGenAmount] = useState<number>(typeof window !== "undefined" ? Number(localStorage.getItem("genAmount")) : 1);
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
